48ac0b9472ac2d5bbfd6f08e7026d751c93a7249
gaLong <1000ji1@naver.com>
2022-11-18 15:26:45 +0900

    home ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Main from './Components/Pages/Main/main';
 import './App.css';
 import Login from './Components/Pages/Login/login/login';
-
+import Home from './Home'
 function App() {
   return (
     <>
-      <Login></Login>
+      <Home></Home>
     </>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <>
+        <div className="bookcover">
+
+            <div className="bookcover-container">
+            <div className="bookcover-left">
+                <div className="bookcover-left-dot">
+                    <div className="bookcover-left-white">
+                        <div className="bookcover-left-content">
+                            아앙아ㅏㅇ 여기에 사용자 사진 추가
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="bookcover-right">
+                <div className="bookcover-right-dot">
+                    <div className="bookcover-right-white">
+                        <div className="couple-name">OOOO의 홈피</div>
+                        <div className="bookcover-right-content">
+                            아아아아 여기에 component 추가
+                        </div>
+                    </div>
+
+                    <div className="menu">
+                        <div className="menu-container">
+                            <a href="##">메인</a>
+                            <a href="##">사진</a>
+                            <a href="##">친구</a>
+                            <a href="##">기념일</a>
+                            <a href="##">설정</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </> 
+    );
+};
+
+export default Home;
