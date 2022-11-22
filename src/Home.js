c8345bd082dc9002355a7e4c08b0188024fe6710
gahyeon <1000ji1@naver.com>
2022-11-22 20:50:20 +0900

    중첩 라우터 사용

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './Home.css';
+import { Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import {Link} from "react-router-dom";
+import Main from './Components/Pages/Main/main'
+import Photo from './Components/Pages/Photo/post/post'
+import Friend from './Components/Pages/Friend/searchFriend'
+import Anniversary from './Components/Pages/Anniversary/anniversary'
+import Setting from './Components/Pages/Setting/editUserInformation'
 
 const Home = () => {
     return (
@@ -12,7 +19,17 @@ const Home = () => {
                 <div className="bookcover-left-dot">
                     <div className="bookcover-left-white">
                         <div className="bookcover-left-content">
-                            아앙아ㅏㅇ 여기에 사용자 사진 추가
+                            
+                            <div className="userImg-container">
+                                <div className="userImg">
+                                    <div className="boyImg">sdf</div>
+                                    <div className="connect"></div>
+                                    <div className="connect"></div>
+                                    <div className="connect"></div>
+                                    <div className="girlImg">dddd</div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
@@ -22,18 +39,18 @@ const Home = () => {
                 <div className="bookcover-right-dot">
                     <div className="bookcover-right-white">
                         <div className="couple-name">OOOO의 홈피</div>
-                        <div className="bookcover-right-content">
-                            아아아아 여기에 component 추가
+                        <div className="bookcover-right-content">              
+                            <Outlet />
                         </div>
                     </div>
 
                     <div className="menu">
                         <div className="menu-container">
-                            <Link to="/">메인</Link>
-                            <Link to="/photo">사진</Link>
-                            <Link to="/friend">친구</Link>
-                            <Link to="/anniversary">기념일</Link>
-                            <Link to="/setting">설정</Link>
+                            <Link to="/home">메인</Link>
+                            <Link to="/home/photo">사진</Link>
+                            <Link to="/home/friend">친구</Link>
+                            <Link to="/home/anniversary">기념일</Link>
+                            <Link to="/home/setting">설정</Link>
                         </div>
                     </div>
                 </div>
