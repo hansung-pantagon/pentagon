266c34a8be8ee9d525db20a3a860fb39e62c3f42
e2huh <e2huh321@gmail.com>
2022-11-23 14:05:07 +0900

    setting 수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Main from './Components/Pages/Main/main';
 import './App.css';
 import Login from './Components/Pages/Login/login/login';
+import Setting from './Components/Pages/Setting/editUserInformation'
 
 function App() {
   return (
     <>
-      <Login></Login>
+      <Setting></Setting>
     </>
   );
 }
