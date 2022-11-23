3c72e2bad17251a1ac91efa0e75f1e078b8793ca
e2huh <e2huh321@gmail.com>
2022-11-23 14:11:40 +0900

    setting 구현

--- src/Components/Pages/Setting/editUserInformation.js
+++ src/Components/Pages/Setting/editUserInformation.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './editUserInformation.css';
+import SetSetting from './setting';
+import SetTheme from './setTheme';
 
-const editUserInformation = () => {
+function EditUserInformation () {
+    const [content, setContent] = useState(false);
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="background">
+                <div className="left-box">
+                    <button onClick={() => setContent(true)}>설정</button>
+                    <button onClick={()=>setContent(false)}>테마</button>
+                    <button onClick={()=>setContent(false)}>공유하기</button>
+                    <button onClick={()=>setContent(false)}>로그아웃</button>
+                </div>
+                <div className="right-box">
+                    {content ? <SetSetting/> : <SetTheme/>}
+                </div>
+            </div>
+        </>
     );
 };
 
-export default editUserInformation;
+export default EditUserInformation;
