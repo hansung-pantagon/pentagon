152dd8492f6eab304a679d5c659ca7b505648f5e
gaLong <1000ji1@naver.com>
2022-11-23 15:29:18 +0900

    post css  구현

--- src/Components/Pages/Photo/post/post.js
+++ src/Components/Pages/Photo/post/post.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import './post.css'
+import AddButton from '../images/addButton.png';
+import ODung from '../images/odung2.jpeg';
+import {Link} from "react-router-dom";
 
 const post = () => {
     return (
-        <div>
+        <div className="post-container">
+            <div className="post">
+                <div className="post-grid">
+                    <div className="post-content-container">
+                        <div className="postImg-border"><img src={ODung} className="postImg"></img></div>
+                        <div className="postDate">2022.10.05</div>
+                        <div className="postContent">10월 달이다!!!</div>
+                    </div>
+                </div>
+                <div className="post-grid">
+                    <div className="post-content-container">
+                        <div className="postImg-border"><img src={ODung} className="postImg"></img></div>
+                        <div className="postDate">2022.10.05</div>
+                        <div className="postContent">10월 달이다!!!</div>
+                    </div>
+                </div>
+                <div className="post-grid">
+                    <div className="post-content-container">
+                        <div className="postImg-border"><img src={ODung} className="postImg"></img></div>
+                        <div className="postDate">2022.10.05</div>
+                        <div className="postContent">10월 달이다!!!</div>
+                    </div>
+                </div>
+                <Link to="/home/photo/writePost">
+                    <div className="post-grid"><img className="addButton" src={AddButton}></img></div>
+                </Link>
+            </div>
             
         </div>
     );
