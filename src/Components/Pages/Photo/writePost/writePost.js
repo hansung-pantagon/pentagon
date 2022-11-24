b0dfb5f8a24a4c9fb5ccb8cd3df679cefb6c05da
gahyeon <1000ji1@naver.com>
2022-11-24 16:54:11 +0900

    글 작성 css

--- src/Components/Pages/Photo/writePost/writePost.js
+++ src/Components/Pages/Photo/writePost/writePost.js
@@ -6,15 +6,22 @@ const writePost = () => {
     return (
         <div className="writePost-container">
             <form action="">
-                <div className="writePost-up">
-                    <div className="date">날짜<input type="date"></input></div>
-                    <div className="upload">업로드</div>
-                    <Link to="/home/photo"><div className="cancel">취소</div></Link>
+                <div>
+                    <div className="date">날짜 <input type="date"></input></div>
+                    <div>
+                        <Link to="/home/photo"><div className="cancel">취소</div></Link>
+                        
+                        {/*글 작성 처리*/}
+                        <div className="upload">업로드</div>
+                    </div>
+                    
                 </div>
                 <div className="writePost-down">
                     <div className="writePost-content-container">
                         <div>
-                            <div className="writePost-img"><input type="file"></input></div>
+                            <input type="file" className="writePost-img"></input>
+                        </div>
+                        <div>
                             <input className="writePost-content" type="text"></input>
                         </div>
                         
