2eacad22093cafe66f4178f290dd34b14528d6ce
gahyeon <1000ji1@naver.com>
2022-11-24 17:11:57 +0900

    라우터에 eachPost 추가

--- src/Components/Pages/Photo/eachPost/eachPost.js
+++ src/Components/Pages/Photo/eachPost/eachPost.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 const eachPost = () => {
     return (
-        <div>
+        <div className="writePost-container">
+            <form action="">
+                <div>
+                    <div className="date">날짜 <input type="date"></input></div>
+                    <div>
+                        <Link to="/home/photo"><div className="cancel">취소</div></Link>
+                        
+                        {/*글 작성 처리*/}
+                        <div className="upload">업로드</div>
+                    </div>
+                    
+                </div>
+                <div className="writePost-down">
+                    <div className="writePost-content-container">
+                        <div>
+                            <input type="file" className="writePost-img"></input>
+                        </div>
+                        <div>
+                            <input className="writePost-content" type="text"></input>
+                        </div>
+                        
+                    </div>
+                </div>
+            </form>
             
         </div>
     );
