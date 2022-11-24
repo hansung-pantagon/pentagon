409f700d993c843f684af382cda9bc33be403cbf
gahyeon <1000ji1@naver.com>
2022-11-24 18:15:15 +0900

    eachPost css 완료

--- src/Components/Pages/Photo/eachPost/eachPost.js
+++ src/Components/Pages/Photo/eachPost/eachPost.js
@@ -5,29 +5,25 @@ import {Link} from "react-router-dom";
 const eachPost = () => {
     return (
         <div className="writePost-container">
-            <form action="">
                 <div>
-                    <div className="date">날짜 <input type="date"></input></div>
+                    <div className="date">2022년 10월 1일</div>
                     <div>
-                        <Link to="/home/photo"><div className="cancel">취소</div></Link>
+                        <Link to="/home/photo"><div className="cancel">이전</div></Link>
                         
-                        {/*글 작성 처리*/}
-                        <div className="upload">업로드</div>
                     </div>
                     
                 </div>
                 <div className="writePost-down">
                     <div className="writePost-content-container">
                         <div>
-                            <input type="file" className="writePost-img"></input>
+                            <img src="" type="file" className="writePost-img"></img>
                         </div>
                         <div>
-                            <input className="writePost-content" type="text"></input>
+                            <div className="writePost-content" type="text"> // 여기에 내용</div>
                         </div>
                         
                     </div>
                 </div>
-            </form>
             
         </div>
     );
