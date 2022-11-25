e84b5ae0c85f249d0a277587ca8dd16a24564d10
e2huh <e2huh321@gmail.com>
2022-11-25 14:02:26 +0900

    setting 수정

--- src/Components/Pages/Setting/setTheme.js
+++ src/Components/Pages/Setting/setTheme.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './setTheme.css';
+import Color from './Color';
+import { useState } from "react";
 
 const SetTheme = () => {
+    const [color, setColor] = useState();
     return (
         <>
             <div>테마</div>
                 <div className="setting">
                     <div>바탕</div>
                     <div className ="theme-box">
-                        <div className = "pink"></div>
+                        <div className = "pink" onclick={()=>setColor()} ></div>
                         <div className = "skyblue"></div>
                         <div className = "lightgreen"></div>
                         <div className = "lightgrey"></div>
