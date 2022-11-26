e64cfe4cea2b5b92934d3f6fa3223a5f3cf7e10f
acceptor-gyu <gseonggyu968@gmail.com>
2022-11-27 03:50:34 +0900

    setting update

--- src/Components/Pages/Setting/editUserInformation.js
+++ src/Components/Pages/Setting/editUserInformation.js
@@ -4,12 +4,51 @@ import SetSetting from './setting';
 import SetTheme from './setTheme';
 
 function EditUserInformation (props) {
+
+    const users = [];
+
+    const dummyData = {
+        boy: {
+            birthday: 19990402,
+            name: "name",
+            imgUrl: "imgUrl"
+        },
+        girl: {
+            birthday: 19990402,
+            name: "name",
+            imgUrl: "imgUrl"
+        },
+        id: "aaa@aaa.com",
+        idImgUrl: "",
+        meetAt: "20221111",
+        nickname: "nickname",
+        pw: "aaa"
+    }
+
+    users.push(dummyData);
+
+    window.localStorage.setItem("user", JSON.stringify(users));
+
     const [content, setContent] = useState(false);
+    // SetSetting 컴포넌트에 넘겨줄 데이터 정의
 
     const [bgColor, setBgColor] =useState("#298FA6");
     const [themeColor, setThemeColor] =useState("#47ACC2");
 
-    const getColorValue = (bgColor,themeColor)=>{ //여기서 전달 받았음
+    // 로컬의 전체 유저 데이터 가져오는 함수
+    const getLocalUsers = JSON.parse(window.localStorage.getItem("user"));
+    // 현재 로그인한 id(더미 데이터) / 세션 스토리지에서 가져오는 것
+    const nowLogIn = "aaa@aaa.com";
+
+    // 현재 로그인한 id 정보 가져오기, 구조분해할당으로 배열을 객체로 만듦
+    const [userInfo] = getLocalUsers.filter(data => data.id === nowLogIn);
+
+    console.log([userInfo]);
+    console.log(getLocalUsers);
+
+
+
+    const getColorValue = (bgColor,themeColor) => { //여기서 전달 받았음
         setBgColor(bgColor);
         setThemeColor(themeColor);
         console.log("editUserInformation에서 ",bgColor);
@@ -21,13 +60,21 @@ function EditUserInformation (props) {
         <>
             <div>
                 <div className="left-box">
-                    <button onClick={() => setContent(true)}>설정</button>
-                    <button onClick={()=>setContent(false)}>테마</button>
-                    <button onClick={()=>setContent(false)}>공유하기</button>
-                    <button onClick={()=>setContent(false)}>로그아웃</button>
+                    <button onClick={() => setContent(true)}>
+                        설정
+                    </button>
+                    <button onClick={() => setContent(false)}>
+                        테마
+                    </button>
+                    <button onClick={()=>setContent(false)}>
+                        공유하기
+                    </button>
+                    <button onClick={()=>setContent(false)}>
+                        로그아웃
+                    </button>
                 </div>
                 <div className="right-box">
-                    {content ? <SetSetting/> : <SetTheme getColorValue={getColorValue}/>}
+                    {content ? <SetSetting userInfo={userInfo} /> : <SetTheme getColorValue={getColorValue}/>}
                 </div>
             </div>
         </>
--- src/Components/Pages/Setting/setting.js
+++ src/Components/Pages/Setting/setting.js
@@ -1,39 +1,114 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './editUserInformation.css';
 
-const SetSetting = () => {
+const SetSetting = ( {userInfo} ) => {
+
+    // nickname의 값이 바뀔때마다 화면에 리렌더링하면서 보여주기 위해 useState 사용
+    const [edited, setEdited] = useState(userInfo);
+
+    // 로컬 스토리지에서 user 데이터 전부를 가져옴 (현재는 문자열의 형태)
+    const getData = window.localStorage.getItem("user");
+
+    // 바뀐 데이터를 로컬 스토리지에 저장하는 함수
+    const handleSave = data => {
+        const { boy, girl, id, idImgUrl, meetAt, nickname, pw } = data;
+        
+        const editArray = JSON.parse(getData).map(row => data.id === "aaa@aaa.com" ? {
+            boy: {
+                birthday: boy.name,
+                imgUrl: boy.imgUrl,
+                name: boy.name,
+            },
+            girl: {
+                birthday: girl.name,
+                imgUrl: girl.imgUrl,
+                name: girl.name,
+            },
+            id,
+            idImgUrl,
+            meetAt,
+            nickname,
+            pw
+        } : row)
+        console.log(editArray);
+        window.localStorage.setItem("user", JSON.stringify(editArray));
+    }
+
+    const onChange = (e) => {
+        const {name, value } = e.target;
+        console.log(name);
+        console.log(value);
+        console.log(edited);
+
+        setEdited({
+            ...edited,
+            [name]: value
+        })
+        console.log(edited);
+    }
+
+    const onSubmitEdit = (e) => {
+        e.preventDefault();
+        console.log(e.target);
+        handleSave(edited);
+    }
+
     return (
         <>
             <div>설정</div>
-            <div className="setting">
-                <div>남자</div>
-                <div className ="setting-box">
-                    <div>이름
-                        <input type="text" placeholder=" "></input>
-                    </div>
-                    <div>생년월일
-                        <input type="text" placeholder=" "></input>
+                <form onSubmit={onSubmitEdit}>
+                    <div className="setting">
+                        <div>남자</div>
+                        <div className ="setting-box">
+                            <div>
+                                이름
+                                <div>{userInfo.boy.name}</div>
+                            </div>
+                            <div>생년월일
+                                <div>{userInfo.boy.birthday}</div>
+                            </div>
+                            <div>
+                                <img
+                                    id="boyImg"
+                                    src={userInfo.boy.imgUrl}
+                                    alt="이미지가 없습니다.">
+                                </img>
+                            </div>
+                        </div>
+                        <div>여자</div>
+                        <div className ="setting-box">
+                            <div>
+                                이름
+                                <div>{userInfo.girl.name}</div>
+                            </div>
+                            <div>생년월일
+                                <div>{userInfo.girl.birthday}</div>
+                            </div>
+                            <div>
+                                <img
+                                    id="boyImg"
+                                    src={userInfo.boy.imgUrl}
+                                    alt="이미지가 없습니다.">
+                                </img>
+                            </div>
+                        </div>
+                    <div className = "info">
+                        <div>사귄 날짜
+                            <div>{userInfo.meetAt}</div>
+                        </div>
+                        <div>닉네임
+                            <input
+                                className="nickname"
+                                type="text"
+                                name="nickname"
+                                value={edited.nickname}
+                                onChange={onChange}
+                            />
+                        </div>
                     </div>
                 </div>
-                <div>여자</div>
-                <div className ="setting-box">
-                    <div>이름
-                        <input type="text" placeholder=" "></input>
-                    </div>
-                    <div>생년월일
-                        <input type="text" placeholder=" "></input>
-                    </div>
-                </div>
-                <div className = "info">
-                    <div>사귄 날짜
-                        <input type="text" placeholder=" "></input>
-                    </div>
-                    <div>닉네임
-                        <input type="text" placeholder=" "></input>
-                    </div>
-                </div>
-            </div>
-            <button>설정</button>
+            <button type="submit">설정</button>
+            </form>
         </>
     );
 }
