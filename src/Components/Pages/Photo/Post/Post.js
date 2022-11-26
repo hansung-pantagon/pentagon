dff886430e3e6522ff1ac5ee7b0b68ca6e9a3005
acceptor-gyu <gseonggyu968@gmail.com>
2022-11-27 02:27:16 +0900

    delete unused hooks

--- src/Components/Pages/Photo/Post/Post.js
+++ src/Components/Pages/Photo/Post/Post.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddButton from '../images/addButton.png';
 import { Link } from "react-router-dom";
 import PostData from "./PostData";
