7cd47dfa4a478222a7df1bd2b898849fc1baf40f
gaLong <1000ji1@naver.com>
2022-11-26 23:20:32 +0900

    photo폴더 지움

--- src/Components/Pages/Main/main.js
+++ src/Components/Pages/Main/main.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { useEffect, useState } from "react";
 import './main.css'
 
 const Main = () => {
+    const [loginInfo, setLoginInfo] = useState([]);
+    const [nowLoginInfo, setNowLoginInfo] = useState([]);
+    
+    const nowId = JSON.parse(sessionStorage.getItem("loginId"));
+    console.log("세션에서 가져온 로그인한 id ", nowId);
+
+    useEffect(() => {
+        setLoginInfo(JSON.parse(localStorage.getItem('user')));
+        console.log("로컬에서 가져온 모든 정보 ", loginInfo);
+        // const nowUserInfo = loginInfo.find((user)=>user.id === nowId);
+        // console.log("nowUserInfo : ", nowUserInfo);
+        
+        // if(loginInfo.find((user)=>user.id === nowId)){ //찾으면? 있으면?
+            
+        //     setNowLoginInfo(JSON.parse(localStorage.getItem("user")));
+        // }
+    }, []);
+
+    //console.log("로컬에서 찾은 id의 정보들", nowLoginInfo);
+
+
+
+
     return (
         <div className="CommonImg-container">
             <div className="CommonImg">//img가져오기</div>
