bd46112edbbea0c46e4615fa7eb886fd6df39996
kys0417 <yungsang1111@gmail.com>
2022-11-28 17:07:22 +0900

    userInfo 수정

--- src/Components/Pages/Login/setting/userInformation.js
+++ src/Components/Pages/Login/setting/userInformation.js
@@ -29,10 +29,6 @@ function UserInFormation() {
     idImgUrl: "",
   });
 
-  const boyImgInfo = document.getElementById("boyImgUrl");
-  const girlImgInfo = document.getElementById("girlImgUrl");
-  const idImgInfo = document.getElementById("idImgUrl");
-
   const handleChange = (e) => {
     // input 입력을 받아 form에 저장
     const { name, value } = e.target;
@@ -44,9 +40,7 @@ function UserInFormation() {
   };
 
   const handleSubmit = (e) => {
-
     e.preventDefault();
-<<<<<<< Updated upstream
 
     alert(`${signUpId} 님 회원가입 완료.`);
     navigate("/signUpInPage/", {
@@ -56,7 +50,7 @@ function UserInFormation() {
         nickName: e.target.nickName.value,
         boyBirthday: boyBirthday,
         boyName: e.target.boyName.value,
-        boyImgUrl: URL.createObjectURL(document.getElementById("boyImgUrl").files[0]), // 이미지는 아직 구현 못함 ㅠㅠ 알던 반칸 전송
+        boyImgUrl: URL.createObjectURL(document.getElementById("boyImgUrl").files[0]),
         girlBirthday: girlBirthday,
         girlName: e.target.girlName.value,
         girlImgUrl: URL.createObjectURL(document.getElementById("girlImgUrl").files[0]),
@@ -64,45 +58,6 @@ function UserInFormation() {
         idImgUrl: URL.createObjectURL(document.getElementById("idImgUrl").files[0]),
       },
     });
-=======
-    
-    setLocalInfo(() => {
-      // localStorage에 저장하는 useState
-      // 새로고침을 하고 setitem을 했을때, localstorage 값 유지
-      if (typeof window !== "undefined") {
-        const saved = window.localStorage.getItem("user");
-        if (saved !== null) {
-          return JSON.parse(saved);
-        } else {
-          return [];
-        }
-      }
-    });
-    //회원가입 하려는 id가 localstorage에 존재하는지 검증하는 if문
-    if (localInfo.find((user) => user?.id === signUpId)) {
-      alert("동일한 ID가 존재합니다 다시 입력해주세요");
-      navigate("/signUpInPage/signup");
-    } else {
-      // 없다면 입력한 정보를 login 페이지로 옮겨준다
-      // 왜 여기서 저장안하고 navigate 안씀? => 저장하는 코드를 쓰고 navigate를 쓰면 localstorage에 저장이 안되고 navigate됨
-      alert(`${signUpId} 님 회원가입 완료.`);
-      navigate("/signUpInPage/", {
-        state: {
-          id: signUpId,
-          pw: signUpPw,
-          nickName: e.target.nickName.value,
-          boyBirthday: boyBirthday,
-          boyName: e.target.boyName.value,
-          boyImgUrl: URL.createObjectURL(document.getElementById("boyImgUrl").files[0]), // 이미지는 아직 구현 못함 ㅠㅠ 알던 반칸 전송
-          girlBirthday: girlBirthday,
-          girlName: e.target.girlName.value,
-          girlImgUrl: URL.createObjectURL(document.getElementById("girlImgUrl").files[0]),
-          meetAt: whenMeetAt,
-          idImgUrl: URL.createObjectURL(document.getElementById("idImgUrl").files[0]),
-        },
-      });
-    }
->>>>>>> Stashed changes
     setForms({
       id: "",
       pw: "",
@@ -127,17 +82,7 @@ function UserInFormation() {
           <div className="boy-input-div">
             <input type="text" name="boyName" value={forms.boyName} onChange={handleChange} placeholder="이름" required minLength={1}></input>
             <DatePicker selected={boyBirthday} name="boybirthday" placeholderText="생년월일" onChange={(date) => setBoyBirthday(date)} locale={ko} dateFormat="yyyy.MM.dd" />
-<<<<<<< Updated upstream
             <input type="file" id="boyImgUrl" className="boyImgUrl" name="boyImgUrl" accept=".jpg, jpeg, .png" onChange={handleChange} />
-=======
-            <input
-              type="file"
-              id="boyImgUrl"
-              name="boyImgUrl"
-              accept=".jpg, jpeg, .png"
-              onChange={handleChange}
-            />
->>>>>>> Stashed changes
           </div>
         </div>
         <div className="girl-div">
@@ -145,32 +90,12 @@ function UserInFormation() {
           <div className="girl-input-div">
             <input type="text" name="girlName" value={forms.girlName} onChange={handleChange} placeholder="이름" required minLength={1}></input>
             <DatePicker selected={girlBirthday} name="girlbirthday" placeholderText="생년월일" onChange={(date) => setGirlBirthday(date)} locale={ko} dateFormat="yyyy.MM.dd" />
-<<<<<<< Updated upstream
             <input type="file" id="girlImgUrl" className="girlImgUrl" name="girlImgUrl" accept=".jpg, jpeg, .png" onChange={handleChange} />
-=======
-            <input
-              type="file"
-              id="girlImgUrl"
-              name="girlImgUrl"
-              accep=".jpg, jpeg, .png"
-              onChange={handleChange}
-            />
->>>>>>> Stashed changes
           </div>
         </div>
         <div className="last-input-div">
           <DatePicker selected={whenMeetAt} name="meetAt" placeholderText="사귄날짜" onChange={(date) => setWhenMeetAt(date)} locale={ko} dateFormat="yyyy.MM.dd" />
-<<<<<<< Updated upstream
           <input type="file" id="idImgUrl" className="idImgUrl" name="idImgUrl" accept=".jpg, jpeg, .png" onChange={handleChange} />
-=======
-          <input
-              type="file"
-              id="idImgUrl"
-              name="idImgUrl"
-              accep=".jpg, jpeg, .png"
-              onChange={handleChange}
-            />
->>>>>>> Stashed changes
           <input type="text" name="nickName" value={forms.nickName} onChange={handleChange} placeholder="닉네임" required minLength={1}></input>
         </div>
         <button type="submit" className="setting-bt">
